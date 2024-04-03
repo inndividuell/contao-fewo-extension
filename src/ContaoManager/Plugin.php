d4d1341423d909684ab928db89615196b6f5892f
stefanperetzki <spe@ndcom.de>
2024-04-03 12:59:54 +0200

    update

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-namespace Inndividuell\ContaoPhpVersion\ContaoManager;
+namespace Inndividuell\ContaoFewoExtension\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
