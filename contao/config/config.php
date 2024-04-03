b50f7c9b23cf6c23d2b11689548259f50957507d
stefanperetzki <spe@ndcom.de>
2024-04-03 13:01:31 +0200

    update

--- contao/config/config.php
+++ contao/config/config.php
@@ -12,16 +12,3 @@ $GLOBALS['BE_MOD']['inn_fewo']['inn_fewo_furnishing'] = array(
     'tables' => array('tl_inn_fewo_furnishing')
 );
 
-/**
- * Front end modules
- */
-//$GLOBALS['FE_MOD']['perisoft'] = array
-//(
-//    'pp_project_view'     => 'ModulePpProjectView',
-//    'pp_project_view_active'     => 'ModulePpProjectViewActive',
-//    'pp_project_view_finished'     => 'ModulePpProjectViewFinished',
-//    'pp_project_view_partner'     => 'ModulePpProjectViewPartner',
-//    'pp_project_where_plant'     => 'ModulePpProjectWherePlant',
-//    'pp_project_gallery'     => 'ModulePpProjectGallery',
-//    'pp_update_counters'     => 'ModulePpUpdateCounters',
-//);
