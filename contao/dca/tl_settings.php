797bd3edc6fad1bde058449fb53599182638da70
stefanperetzki <spe@ndcom.de>
2024-08-01 16:39:27 +0200

    update

---
+++ contao/dca/tl_settings.php
@@ -0,0 +1,42 @@
+<?php
+//$GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace(';{chmod_legend', '{fewo_legend:hide},fewoPriceColumns,fewoPriceRows;{chmod_legend}', $GLOBALS['TL_DCA']['tl_page']['palettes']['root']);
+
+$replaceString = '{fewo_legend:hide},fewoPriceColumns,fewoPriceRows';
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace(
+    ';{chmod_legend'
+    ,   $replaceString.';{chmod_legend'
+    ,   $GLOBALS['TL_DCA']['tl_page']['palettes']['root']
+);
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['fewoPriceColumns'] = array
+(
+    'inputType' => 'multiColumnWizard',
+    'eval' => array
+    (
+        'columnFields' => array
+        (
+            'text' => array
+            (
+                'label' => &$GLOBALS['TL_LANG']['tl_table_name']['text'],
+                'inputType' => 'textarea',
+            )
+        )
+    ),
+    'sql' => "blob NULL"
+);
+$GLOBALS['TL_DCA']['tl_settings']['fields']['fewoPriceRows'] = array
+(
+    'inputType' => 'multiColumnWizard',
+    'eval' => array
+    (
+        'columnFields' => array
+        (
+            'text' => array
+            (
+                'label' => &$GLOBALS['TL_LANG']['tl_table_name']['text'],
+                'inputType' => 'textarea',
+            )
+        )
+    ),
+    'sql' => "blob NULL"
+);
